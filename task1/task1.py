20385c8792f63584955f70e7923e4d42db96e4f4
term51 <webworktoday51@gmail.com>
2025-05-22 16:58:11 +0300

    Implemented input of values via command line arguments

--- task1/task1.py
+++ task1/task1.py
@@ -1,25 +1,29 @@
-n = int(input("Enter N - length of circular array: "))
-m = int(input("Enter M - interval length: "))
+import sys
 
-circular_array = [i for i in range(1, n + 1)]
-i = 0
-part_of_path = []
-full_path = []
 
-while True:
-    next_index = i % len(circular_array)
-    part_of_path.append(circular_array[next_index])
+def main(n, m):
+    circular_array = [i for i in range(1, n + 1)]
+    i = 0
+    part_of_path = []
+    full_path = []
 
-    if len(part_of_path) == m:
-        i -= 1
-        full_path.append(part_of_path[0])
+    while True:
+        next_index = i % len(circular_array)
+        part_of_path.append(circular_array[next_index])
 
-        if part_of_path[-1] == circular_array[0]:
-            break
+        if len(part_of_path) == m:
+            i -= 1
+            full_path.append(part_of_path[0])
 
-        part_of_path = []
+            if part_of_path[-1] == circular_array[0]:
+                break
 
-    next_element = circular_array[next_index]
-    i += 1
+            part_of_path = []
 
-print("".join(str(x) for x in full_path))
+        next_element = circular_array[next_index]
+        i += 1
+
+    print("".join(str(x) for x in full_path))
+
+
+main(int(sys.argv[1]), int(sys.argv[2]))
